73f30c6f385ea2fae1fabe8389427718afd9b94a
spooky squirrel <one.spooky.squirrel@gmail.com>
2020-05-10 12:39:24 -0700

    Initial Commit

---
+++ udacity-chatbot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  udacity-chatbot
+//
+//  Created by nanaster on 5/10/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
