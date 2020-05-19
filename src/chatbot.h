f717bc82f1fe23ea2d4d10b468f6fc51e8d4291c
spooky squirrel <one.spooky.squirrel@gmail.com>
2020-05-18 17:46:28 -0700

    Init commit

---
+++ src/chatbot.h
@@ -0,0 +1,55 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+    private:
+        // data handles (owned)
+        wxBitmap *_image; // avatar image
+
+        // data handles (not owned)
+        GraphNode *_currentNode;
+        GraphNode *_rootNode; /* root node important when reaching deadend */
+        ChatLogic *_chatLogic;
+
+        // proprietary functions
+        /* number of characters to change to covert string one to sting two
+         the one with the smallest levensthtein distance is the best fit answer
+         */
+        int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+    public:
+        // constructors / destructors
+        ChatBot();                     // constructor WITHOUT memory allocation
+        ChatBot(std::string filename); // constructor WITH memory allocation
+        ~ChatBot();                    // destructor
+
+        //// STUDENT CODE
+        ////
+        int testInt;
+        
+        ChatBot(const ChatBot &source);             // copy constructor
+        ChatBot &operator=(const ChatBot &source);   //copy assignment operator
+        ChatBot(ChatBot &&source);                  // move constructor
+        ChatBot &operator=(ChatBot &&source);       // move assignment operator
+        
+        ////
+        //// EOF STUDENT CODE
+
+        // getters / setters
+        void SetCurrentNode(GraphNode *node);
+        void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+        void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+        wxBitmap *GetImageHandle() { return _image; }
+
+        // communication
+        void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
---
+++ src/graphnode.cpp
@@ -0,0 +1,92 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+#include<iostream> // temporary
+
+GraphNode::GraphNode(int id)
+{
+    //std::cout << "New GraphNode constructor" << std::endl;
+    //_chatBot = std::make_shared<ChatBot>();
+    _chatBot = std::make_unique<ChatBot>();
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot;  // chatbot is now unique pointers
+    _chatBot = nullptr;
+
+    // delete edges (child and parent)
+    for (auto& it : _childEdges)
+    {
+        //std::cout << "Delete one childedge " << it << std::endl;
+        it = nullptr;
+    }
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+/* happens only a single time during load answergraph */
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    //_childEdges.push_back(edge);
+    _childEdges.emplace_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+/*void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    _chatBot = chatbot;
+    _chatBot->SetCurrentNode(this);
+}
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+*/
+
+
+void GraphNode::MoveChatbotHere(ChatBot &&chatbot)
+{
+    //std::cout << "MoveChatbotHere method" << std::endl;
+    *_chatBot = std::move(chatbot);
+    _chatBot->SetCurrentNode(this);
+}
+
+
+// tell chatbot to move from current node to new node
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    //std::cout << "MoveChatbotToNewNode method" << std::endl;
+    newNode->MoveChatbotHere(std::move(*_chatBot));
+}
+
+
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
